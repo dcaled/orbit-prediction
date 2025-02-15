6cca985cd2c945f0884586ce9fa6543b4cfc5c40
dcaled <dcaled@gmail.com>
2025-02-15 11:34:10 +0000

    Change endpoint to return a list of predictions instead of a single prediction.

--- model_api/app.py
+++ model_api/app.py
@@ -1,33 +1,55 @@
+import random
+from typing import List
+
 from fastapi import FastAPI
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, field_validator
 from datetime import datetime
 
 app = FastAPI()
 
 
-class PositionRequest(BaseModel):
+class ObjectPositionRequest(BaseModel):
     """
     Input model for requesting satellite position.
 
     Attributes:
-        timestamp (datetime): The datetime for which the satellite position is requested.
+        space_object_id (str): The identifier of the space object.
+        number_of_predictions (int): Number of predictions.
     """
-    timestamp: datetime = Field(..., description="The datetime for which the satellite position is requested.")
+    space_object_id: str = Field(..., description="The identifier of the space-object.")
+    number_of_predictions: int = Field(10, description="Number of predictions.")
 
+    @field_validator("space_object_id")
+    def validate_space_object_id(cls, value):
+        allowed_ids = ["larets"]
+        if value.lower() not in allowed_ids:
+            raise ValueError(f"Invalid space_object_id: {value}. Allowed values: {allowed_ids}")
+        return value
 
-class PositionResponse(BaseModel):
+class ObjectPosition(BaseModel):
     """
     Output model containing the satellite's position.
 
     Attributes:
+        epoch (datetime): The datetime for which the satellite position is computed.
         x (float): The x-coordinate of the satellite.
         y (float): The y-coordinate of the satellite.
         z (float): The z-coordinate of the satellite.
     """
+    epoch: datetime = Field(..., description="The datetime for which the satellite position is computed.")
     x: float = Field(..., description="The x-coordinate of the satellite.")
     y: float = Field(..., description="The y-coordinate of the satellite.")
     z: float = Field(..., description="The z-coordinate of the satellite.")
 
+
+class ObjectPositionList(BaseModel):
+    """
+    Output model containing a list of satellite positions.
+
+    Attributes:
+        positions (List[ObjectPosition]): A list of satellite positions at different timestamps.
+    """
+    positions: List[ObjectPosition]
 
 
 @app.get("/")
@@ -35,21 +57,23 @@ async def root():
     return {"message": "Hello World"}
 
 
-@app.post("/satellite/position", response_model=PositionResponse)
-def get_satellite_position(request: PositionRequest):
+@app.post("/space-object/position", response_model=ObjectPositionList)
+def get_space_object_position(request: ObjectPositionRequest):
     """
     Endpoint to compute satellite position based on input datetime.
 
     Args:
-        request (PositionRequest): Input object containing timestamp.
+        request (PositionRequest): Input object containing space object ID and number of predictions.
 
     Returns:
-        PositionResponse: Object containing x, y, and z coordinates.
+        ObjectPositionList: List of positions containing epoch, x, y, and z coordinates.
     """
-    # Dummy logic to generate x, y, z values based on timestamp
-    timestamp_seconds = request.timestamp.timestamp()  # Convert datetime to seconds
-    x = timestamp_seconds % 10000
-    y = (timestamp_seconds / 2) % 10000
-    z = (timestamp_seconds / 3) % 10000
+    number_of_predictions = request.number_of_predictions
+    positions = []
+    for i in range(number_of_predictions):
+        positions+=[ObjectPosition(epoch=datetime.now(),
+                                   x=random.uniform(1, 2),
+                                   y=random.uniform(1, 3),
+                                   z=random.uniform(1, 4))]
 
-    return PositionResponse(x=x, y=y, z=z)
+    return ObjectPositionList(positions=positions)
