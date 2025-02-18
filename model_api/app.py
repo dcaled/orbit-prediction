271a6e62c00201451a7e8b8901d81ad652d44f13
dcaled <dcaled@gmail.com>
2025-02-18 08:20:25 +0000

    Improve readability

--- model_api/app.py
+++ model_api/app.py
@@ -1,3 +1,4 @@
+from http.client import HTTPException
 from typing import List
 
 from fastapi import FastAPI
@@ -6,13 +7,14 @@ from datetime import datetime
 
 from model_api.inference import OrbitInference
 
+# Initialize inference model
 orbit_inference = OrbitInference(config_path="config.yaml")
 app = FastAPI()
 
 
 class ObjectPositionRequest(BaseModel):
     """
-    Input model for requesting satellite position.
+    Input model for requesting space-object position.
 
     Attributes:
         space_object_id (str): The identifier of the space object.
@@ -24,16 +26,16 @@ class ObjectPositionRequest(BaseModel):
         vel_z (float): The velocity of the space-object in the axis z.
     """
     space_object_id: str = Field(..., description="The identifier of the space-object.")
-    epoch: datetime = Field(..., description="The datetime for which the satellite position is computed.")
-    pos_x: float = Field(..., description="The x-coordinate of the satellite.")
-    pos_y: float = Field(..., description="The y-coordinate of the satellite.")
-    pos_z: float = Field(..., description="The z-coordinate of the satellite.")
-    vel_x: float = Field(..., description="The velocity of the space-object in the axis x.")
-    vel_y: float = Field(..., description="The velocity of the space-object in the axis y.")
-    vel_z: float = Field(..., description="The velocity of the space-object in the axis z.")
+    epoch: datetime = Field(..., description="The datetime for which the space-object position is computed.")
+    pos_x: float = Field(..., description="The x-coordinate of the space-object.")
+    pos_y: float = Field(..., description="The y-coordinate of the space-object.")
+    pos_z: float = Field(..., description="The z-coordinate of the space-object.")
+    vel_x: float = Field(..., description="The velocity of the space-object in the x-axis.")
+    vel_y: float = Field(..., description="The velocity of the space-object in the y-axis.")
+    vel_z: float = Field(..., description="The velocity of the space-object in the z-axis.")
 
     @field_validator("space_object_id")
-    def validate_space_object_id(cls, value):
+    def validate_space_object_id(cls, value: str) -> str:
         allowed_ids = ["larets"]
         if value.lower() not in allowed_ids:
             raise ValueError(f"Invalid space_object_id: {value}. Allowed values: {allowed_ids}")
@@ -67,11 +69,6 @@ class ObjectPositionList(BaseModel):
     positions: List[ObjectPosition]
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-
 @app.post("/space-object/position", response_model=ObjectPositionList)
 def get_space_object_position(request: ObjectPositionRequest):
     """
@@ -84,7 +81,7 @@ def get_space_object_position(request: ObjectPositionRequest):
         ObjectPositionList: List of positions containing epoch, x, y, and z coordinates.
     """
     last_position = {
-        "epoch": request.epoch.timestamp()*1000,
+        "epoch": request.epoch.timestamp()* 1000,  # Convert to milliseconds
         "pos_x": request.pos_x,
         "pos_y": request.pos_y,
         "pos_z": request.pos_z,
@@ -93,15 +90,21 @@ def get_space_object_position(request: ObjectPositionRequest):
         "vel_z": request.vel_z
     }
 
+    try:
+        predictions = orbit_inference.run_inference(last_position)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Inference error: {str(e)}")
+
     future_positions = []
-    predictions = orbit_inference.run_inference(last_position)
     for prediction in predictions:
-        future_positions+=[ObjectPosition(
-            epoch=prediction["epoch"],
-            pos_x=prediction["pos_x"],
-            pos_y=prediction["pos_y"],
-            pos_z=prediction["pos_z"]
-    )]
+        future_positions+=[
+            ObjectPosition(
+                epoch=datetime.fromtimestamp(prediction["epoch"] / 1000),
+                pos_x=prediction["pos_x"],
+                pos_y=prediction["pos_y"],
+                pos_z=prediction["pos_z"]
+            )
+        ]
 
     return ObjectPositionList(
         space_object_id=request.space_object_id,
