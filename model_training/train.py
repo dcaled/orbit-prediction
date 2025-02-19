04459ed633c59e64a695f3ae6d139d300466b549
dcaled <dcaled@gmail.com>
2025-02-19 23:28:04 +0000

    Refactor train.py to use optuna and XGBoost Regressor

--- model_training/train.py
+++ model_training/train.py
@@ -6,18 +6,18 @@
 
 import os
 import sys
-sys.path.insert(0, '..')
+sys.path.insert(0, "..")
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 import json
-from typing import Tuple, Dict
-
 import joblib
 import numpy as np
 import pandas as pd
+import optuna
+from typing import Tuple, Dict, Any
+from xgboost import XGBRegressor
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
-
 from setup_logger import logger
 from utils import load_config
 
@@ -34,7 +34,9 @@ class OrbitPredictionModel:
         self.path_data = self.config["data"]["path_clean_data"]
         self.path_first_position = self.config["models"]["path_first_position"]
         self.path_models = self.config["models"]["path_models"]
+        self.n_trials = self.config["models"]["n_trials"]
         self.models = {}
+        self.best_params = {}
 
 
     def load_orbits_from_folder(self) -> pd.DataFrame:
@@ -57,51 +59,116 @@ class OrbitPredictionModel:
         return pd.json_normalize(orbit_data)
 
 
-    def create_temporal_splits(self, df: pd.DataFrame) -> Tuple[pd.DataFrame, pd.DataFrame]:
+    def process_data(self, df: pd.DataFrame) -> Tuple[pd.DataFrame, pd.DataFrame]:
         """
-        Splits the data temporally into training (80%) and testing (20%) sets.
+        Creates engineered features for model training.
+
+        Args:
+            df (pd.DataFrame): Raw orbit data.
 
         Returns:
-            Tuple[pd.DataFrame, pd.DataFrame]: Training and testing datasets.
+            Tuple[pd.DataFrame, pd.DataFrame]: Feature matrix (X) and target variables (y).
         """
+        # Sort data by epoch to ensure temporal order
         df = df.sort_values("epoch")
         epoch_min = df["epoch"].min()
 
         logger.info("Saving the first training position...")
         self.save_first_position(df[df["epoch"] == epoch_min])
 
-        # Create delta_time as the difference between the record time and the initial time.
-        df["delta_time"] = df["epoch"] - epoch_min
+        # Create delta as the difference between the record time and the initial time.
+        df["delta"] = (df["epoch"] - epoch_min).astype(float)
 
-        split_time = df["epoch"].quantile(0.8)
-        train_data = df[df["epoch"] < split_time]
-        test_data = df[df["epoch"] >= split_time]
+        # Shift positions and velocities to get the last known values
+        shift_features = ["pos_x", "pos_y", "pos_z", "vel_x", "vel_y", "vel_z"]
+        for feature in shift_features:
+            df[f"last_{feature}"] = df[feature].shift(1)
 
-        train_data = train_data.drop(columns=["epoch"])
-        test_data = test_data.drop(columns=["epoch"])
+        # Drop rows with NaN values after shifting
+        df.dropna(inplace=True)
 
-        return train_data, test_data
+        X = df[["delta"] + [f"last_{f}" for f in shift_features]]
+        y = df[["pos_x", "pos_y", "pos_z"]]
+        logger.info(f"Dataset shape: X={X.shape}, y={y.shape}")
+
+        return X, y
 
 
     @staticmethod
-    def create_training_set(df: pd.DataFrame) -> Tuple[pd.DataFrame, np.ndarray, np.ndarray, np.ndarray]:
+    def create_temporal_splits(X: pd.DataFrame, y: pd.DataFrame) -> Tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame]:
         """
-        Prepare the feature matrix X and target vectors y_pos_x, y_pos_y, y_pos_z.
+        Splits the data temporally into training (80%) and validation (20%) sets.
+
+        Args:
+            X (pd.DataFrame): Feature matrix.
+            y (pd.DataFrame): Target values.
 
         Returns:
-            Tuple[pd.DataFrame, np.ndarray, np.ndarray, np.ndarray]: Feature matrix and target values.
+            Tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame]: Train-test split.
         """
 
-        # Drop the last row to align X and Y properly for prediction
-        X = df.iloc[:-1, :]
+        # Temporal split: 80% training, 20% validation
+        split_index = int(0.8 * len(X))
+        X_train, X_val = X[:split_index], X[split_index:]
+        y_train, y_val = y[:split_index], y[split_index:]
+        logger.info(f"Training shape: X={X_train.shape}, y={y_train.shape}")
+        logger.info(f"Validation shape: X={X_val.shape}, y={y_val.shape}")
+        return X_train, X_val, y_train, y_val
 
-        # Target variables are the next position (shifted by one)
-        y_pos_x = df["pos_x"].shift(-1).dropna()
-        y_pos_y = df["pos_y"].shift(-1).dropna()
-        y_pos_z = df["pos_z"].shift(-1).dropna()
 
-        return X, y_pos_x, y_pos_y, y_pos_z
+    def objective(self, trial: optuna.Trial, X_train: pd.DataFrame, y_train: np.ndarray, X_val: pd.DataFrame,
+                  y_val: np.ndarray) -> float:
+        """
+        Objective function for hyperparameter optimization with Optuna.
 
+        Returns:
+            float: Mean Squared Error (MSE) on validation set.
+        """
+        # Define the objective function for Optuna
+        param = {
+            # Number of gradient boosted trees.
+            "n_estimators": trial.suggest_int("n_estimators", 100, 1000),
+            # Boosting learning rate
+            "learning_rate": trial.suggest_float("learning_rate", 0.01, 0.3),
+            # Maximum tree depth for base learners.
+            "max_depth": trial.suggest_int("max_depth", 3, 10),
+            # Subsample ratio of the training instance.
+            "subsample": trial.suggest_float("subsample", 0.5, 1.0),
+            # Subsample ratio of columns when constructing each tree.
+            "colsample_bytree": trial.suggest_float("colsample_bytree", 0.5, 1.0)
+        }
+
+        model = XGBRegressor(objective="reg:squarederror", **param)
+        model.fit(X_train, y_train, eval_set=[(X_val, y_val)], verbose=False)
+
+        return mean_squared_error(y_val, model.predict(X_val))
+
+    def optimize_model_parameters(self, X_train: pd.DataFrame, y_train: np.ndarray, X_val: pd.DataFrame,
+                                  y_val: np.ndarray) -> Dict[str, Any]:
+        """
+        Optimizes hyperparameters using Optuna.
+
+        Returns:
+            Dict[str, Any]: Best hyperparameters found.
+        """
+        study = optuna.create_study(direction="minimize")
+        study.optimize(lambda trial: self.objective(trial, X_train, y_train, X_val, y_val), n_trials=self.n_trials)
+        return study.best_params
+
+    def train_final_model(self, X_train: pd.DataFrame, y_train: np.ndarray, X_val: pd.DataFrame, y_val: np.ndarray,
+                          best_params: Dict[str, Any]) -> XGBRegressor:
+        """
+        Trains the final XGBoost model with the best hyperparameters.
+
+        Returns:
+            XGBRegressor: Trained model.
+        """
+        X_train_full = pd.concat([X_train, X_val], ignore_index=True)
+        y_train_full = np.concatenate([y_train, y_val])
+
+        final_model = XGBRegressor(objective="reg:squarederror", **best_params)
+        final_model.fit(X_train_full, y_train_full)
+        return final_model
 
     def fit_model(self, x_train: pd.DataFrame, y_train_pos_x: np.ndarray, y_train_pos_y: np.ndarray,
                   y_train_pos_z: np.ndarray):
@@ -159,32 +226,35 @@ class OrbitPredictionModel:
         Args:
             first_position (pd.DataFrame): Dataframe containing the first known position of the space-object.
         """
-        first_position.to_json(self.path_first_position, orient='records', lines=True)
+        first_position.to_json(self.path_first_position, orient="records", lines=True)
         logger.info(f"First training position saved to {self.path_first_position}")
 
     def run_pipeline(self):
         """
-        Executes the entire data processing and model training pipeline.
+        Runs the full data pipeline: loading, processing, training, and saving models.
         """
         logger.info("Loading data from storage...")
         df = self.load_orbits_from_folder()
+        if df.empty:
+            logger.error("No data found. Exiting pipeline.")
+            return
+
+        logger.info("Creating features...")
+        X, y = self.process_data(df)
 
         logger.info("Creating temporal splits...")
-        train_data, test_data = self.create_temporal_splits(df)
-        logger.info(f"Training data shape: {train_data.shape}")
-        logger.info(f"Test data shape: {test_data.shape}")
+        X_train, X_val, y_train, y_val = self.create_temporal_splits(X, y)
 
-        logger.info("Creating training and test sets...")
-        x_train, y_train_pos_x, y_train_pos_y, y_train_pos_z = self.create_training_set(train_data)
-        x_test, y_test_pos_x, y_test_pos_y, y_test_pos_z = self.create_training_set(test_data)
+        logger.info("Optimizing and training models...")
+        for axis in ["pos_x", "pos_y", "pos_z"]:
+            logger.info(f"Optimizing model for {axis}...")
+            best_params = self.optimize_model_parameters(X_train, y_train[axis], X_val, y_val[axis])
+            logger.info(f"Best hyperparameters for {axis}: {best_params}")
 
-        logger.info("Training models...")
-        self.fit_model(x_train, y_train_pos_x, y_train_pos_y, y_train_pos_z)
-
-        logger.info("Evaluating models...")
-        mse_scores = self.evaluate(x_test, {"pos_x": y_test_pos_x, "pos_y": y_test_pos_y, "pos_z": y_test_pos_z})
-        logger.info(f"Mean Squared Errors: {mse_scores}")
-
+            logger.info(f"Training final model for {axis} on the full dataset.")
+            final_model = self.train_final_model(X_train, y_train[axis], X_val, y_val[axis], best_params)
+            joblib.dump(final_model, os.path.join(self.path_models, f"model_{axis}.pkl"))
+            logger.info(f"Final model for {axis} saved successfully.")
 
 
 if __name__ == "__main__":
